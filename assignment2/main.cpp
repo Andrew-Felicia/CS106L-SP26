1a2a3f9110187e8d4b01d0edb51ab8a8844380f8
Vincent <yong72426@gmail.com>
2026-07-15 16:41:42 +0800

    finished assignment 2

--- assignment2/main.cpp
+++ assignment2/main.cpp
@@ -13,8 +13,9 @@
 #include <set>
 #include <string>
 #include <unordered_set>
+#include <sstream>
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+std::string kYourName = "Vincent Morgan"; // Don't forget to change this!
 
 /**
  * Takes in a file name and returns a set containing all of the applicant names as a set.
@@ -28,9 +29,45 @@ std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
  * to also change the corresponding functions in `utils.h`.
  */
 std::set<std::string> get_applicants(std::string filename) {
-  // STUDENT TODO: Implement this function.
+  std::ifstream read(filename);
+  if (!read.is_open()) {
+    std::cerr << "can open " << filename << '\n';
+  }
+
+  std::set<std::string> result;
+  std::string line;
+  while (std::getline(read, line)) {
+    if (!line.empty()) {
+      result.insert(line);
+    }
+  }
+  read.close();
+
+  return result;
 }
 
+
+
+bool shareInitials(const std::string& string1, const std::string& string2) {
+  std::stringstream ss1(string1);
+  std::stringstream ss2(string2);
+
+  std::string first1, last1;
+  std::string first2, last2;
+
+  ss1 >> first1 >> last1;
+  ss2 >> first2 >> last2;
+
+  return first1.front() == first2.front() && last1.front() == last2.front();
+}
+
+// bool sameInitials(const std::string& a, const std::string& b) {
+//     auto spaceA = a.find(' ');
+//     auto spaceB = b.find(' ');
+
+//     return a.front() == b.front() &&
+//            a[spaceA + 1] == b[spaceB + 1];
+// }
 /**
  * Takes in a set of student names by reference and returns a queue of names
  * that match the given student name.
@@ -40,7 +77,18 @@ std::set<std::string> get_applicants(std::string filename) {
  * @return          A queue containing pointers to each matching name.
  */
 std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
-  // STUDENT TODO: Implement this function.
+  std::queue<const std::string*> result;
+  if (name == "") {
+    return result;
+  }
+  for (const auto& elem : students) {
+    if (shareInitials(name, elem)) {
+      result.push(&elem);
+    }
+  }
+  return result;
+
+
 }
 
 /**
@@ -54,7 +102,11 @@ std::queue<const std::string*> find_matches(std::string name, std::set<std::stri
  *                Will return "NO MATCHES FOUND." if `matches` is empty.
  */
 std::string get_match(std::queue<const std::string*>& matches) {
-  // STUDENT TODO: Implement this function.
+  if (matches.empty()) {
+    return "NO MATCHES FOUND.";
+  }
+  return *(matches.back());
+  //return "";
 }
 
 /* #### Please don't remove this line! #### */
